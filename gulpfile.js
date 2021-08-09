0b0c5c7823e4197e8150a18a623130f4c6675abe
Goulshat <76894691+Goulshat@users.noreply.github.com>
2021-08-09 20:36:56 +0500

    Правка countries

--- gulpfile.js
+++ gulpfile.js
@@ -150,8 +150,8 @@ exports.reload = reload;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
-  gulp.watch("source/js/**/*.js", gulp.series(scripts));
+  gulp.watch("source/*.html", gulp.series(html,reload));
+  //gulp.watch("source/js/**/*.js", gulp.series(scripts));
 }
 
 exports.default = gulp.series(
